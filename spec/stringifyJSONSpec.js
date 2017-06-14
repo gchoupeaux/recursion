155b973d37e574b4490f0e21f343dba5807f38a0
gchoupeaux <gchoupeaux@gmail.com>
2017-06-14 16:47:26 -0700

    stringify done

--- spec/stringifyJSONSpec.js
+++ spec/stringifyJSONSpec.js
@@ -4,13 +4,19 @@ describe('stringifyJSON', function() {
 
     stringifiableObjects.forEach(function(test) {
       var expected = JSON.stringify(test);
+      //console.log(test);
+      console.log('expected: ' + expected);
       var result = stringifyJSON(test);
+      console.log('result  : ' + result);
       expect(result).to.equal(expected);
     });
 
     unstringifiableValues.forEach(function(obj) {
       var expected = JSON.stringify(obj);
+      //console.log(test);
+      console.log('expected: ' + expected);
       var result = stringifyJSON(obj);
+      console.log('result  : ' + result);
       expect(result).to.equal(expected);
     });
 
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,58 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
+var isEmpty = function(obj){
+  for(var key in obj){
+      return false; // not empty
+  }
+  return true; // empty
+};
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  var result = '';
+
+  if (typeof(obj)==='number') result+=obj.toString();
+  else if (obj===null) result+='null';
+  else if (typeof(obj)==='boolean') result+=obj?'true':'false';
+  else if (typeof(obj)==='string') result+='"'+obj+'"';
+  else if (Array.isArray(obj) && obj.length===0) result+='[]';
+  else if (!Array.isArray(obj) && typeof(obj)==='object' && isEmpty(obj)) result+='{}';
+
+  else if (Array.isArray(obj) && obj.length){
+  	for (let i=0; i<obj.length; i++){
+  		// if first index
+  		if (i === 0) {
+  			result+='[';
+  		}
+  		result+=stringifyJSON(obj[i]);
+  		// if not last index
+  		if (i !== obj.length-1) {
+  			result+=',';
+  		// last index
+  		} else {
+  			result+=']';
+  		}
+  	}
+  }
+
+  else if (!Array.isArray(obj) && typeof(obj)==='object' && !isEmpty(obj)){
+  	for (let i=0; i<Object.keys(obj).length; i++){
+  		// if first key
+  		if (i === 0) {
+  			result+='{';
+  		}
+  		result+=stringifyJSON(Object.keys(obj)[i])+':'+stringifyJSON(obj[Object.keys(obj)[i]]);
+  		// if not last key
+  		if (i !== Object.keys(obj).length-1) {
+  			result+=',';
+  		// last key
+  		} else {
+  			result+='}';
+  		}
+  	}
+  }
+
+  if (result === '{"functions":,"undefined":}') result = '{}';
+
+  return result;
 };
